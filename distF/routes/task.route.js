e147f0fbcfbbe2f103007f3cdb03e06d8b1ac4fc
kuldeep-sonara-pp <kuldeep.sonara@prominentpixel.com>
2025-01-01 19:10:59 +0530

    modify that pakge.json

--- distF/routes/task.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// import { Router } from "express";
-// import checkToken from "../util/auth.middleware";
-// import { createTask } from "../controller/task.controller";
-// const routes = Router();
-// routes.post("/create", checkToken ,createTask);
-// const taskRoutes = routes;
-// export default taskRoutes;
